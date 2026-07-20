0d1b9a1d7b53724f2d2dca4ce637733a86b64514
Mohamad Noval Maulana <maulananoval363@gmail.com>
2026-07-20 14:08:06 +0700

    update icon & name app

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
     id("dev.flutter.flutter-gradle-plugin")
 
     // Add the dependency for the Google services Gradle plugin
-    id("com.google.gms.google-services") version "4.5.0" apply false
+    id("com.google.gms.google-services") version "4.4.4" apply false
 }
 
 android {
